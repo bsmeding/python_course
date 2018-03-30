7ab0002c7e2cbdbb7f0960e36ad7ef4df94152c7
bsmeding <mail@bartsmeding.nl>
2018-03-30 06:36:20 -0700

    updated login to router

--- week2/ex2_simple_telnet_login.py
+++ week2/ex2_simple_telnet_login.py
@@ -2,6 +2,7 @@
 
 import telnetlib
 import getpass
+import time
 from pprint import pprint
 
 TELNET_PORT = 23
@@ -12,13 +13,27 @@ def main():
 	username = 'pyclass'
 	password = getpass.getpass()
 
+
 	remote_conn = telnetlib.Telnet(ip_addr, TELNET_PORT, TELNET_TIMEOUT)
-
-
 	output = remote_conn.read_until("sername:", TELNET_TIMEOUT)
+	# Sent Username
+	remote_conn.write(username + '\n')
+	#Wait for password
+	output = remote_conn.read_until("ssword:", TELNET_TIMEOUT)
+	# Sent Username
+	remote_conn.write(password + '\n')	
 	print output
 
+	#Get login state, sleep 1 second
+	time.sleep(1)
+	output = remote_conn.read_very_eager()
+
+	print output
+
+
+	# Close connection
 	remote_conn.close()
 
 if __name__ == "__main__":
 	main()
+
